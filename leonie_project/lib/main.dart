34046112c1d8072442bdfbb1cab7e006b0c3eec9
Camille Brunin <camille.brunin@gmail.com>
2025-06-07 20:54:26 +0200

    Add a color theme and dark color theme

--- leonie_project/lib/main.dart
+++ leonie_project/lib/main.dart
@@ -2,6 +2,16 @@ import 'package:first_app/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// This is the color theme that will be applied to the whole app
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 187, 37, 70));
+
+// Create the dark color theme
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 187, 37, 70),
+);
+
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
 void main() {
@@ -18,6 +28,19 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Use a default theme provided by flutter and overwrite some selected settings
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        // Style of the AppBar (top bar)
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      // Add a dark mode theme
+      // darkTheme: ThemeData.dark().copyWith(
+      //   colorScheme: kDarkColorScheme,
+      // ),
       home: SplashScreen(),
       navigatorObservers: [routeObserver],
     );
